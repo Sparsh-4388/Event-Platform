bd703065f195c730167827dca6327140e59ea25a
Sparsh <sparshp773@gmail.com>
2025-12-28 18:48:03 +0530

    fixed the api fetch issue

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,8 +5,6 @@ import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import './App.css';
 
-console.log("API BASE URL =", import.meta.env.VITE_API_BASE_URL);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
