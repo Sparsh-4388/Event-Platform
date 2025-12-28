84d6a655269c1926c058d1ad9dc4fccf73103ea0
Sparsh <sparshp773@gmail.com>
2025-12-28 18:42:06 +0530

    debugging the frontend error

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,8 @@ import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import './App.css';
 
+console.log("API BASE URL =", import.meta.env.VITE_API_BASE_URL);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
